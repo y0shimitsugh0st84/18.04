35b22f73c4585512dc4fcbdfe0e282cfebc370d1
y0shimitsugh0st84 <68965220+y0shimitsugh0st84@users.noreply.github.com>
2021-09-28 20:52:39 +0200

    Update tools.sh

--- src/ubuntu/install/tools.sh
+++ src/ubuntu/install/tools.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 ### every exit != 0 fails the script
 set -e
-
+export DEBIAN_FRONTEND=noninteractive
 echo "Install some common tools for further installation"
 apt-get update 
 echo "Install some common tools for further installation ANDROID-----------------------------------------------------"
 apt install git-core gperf bc bison build-essential ccache curl flex g++-multilib gcc-multilib git gnupg gperf imagemagick lib32ncurses5-dev lib32readline-dev lib32z1-dev liblz4-tool libncurses5-dev libsdl1.2-dev libssl-dev libwxgtk3.0-gtk3-dev libxml2 libxml2-utils lzop pngcrush rsync schedtool squashfs-tools xsltproc libc6-dev-i386 x11proto-core-dev libx11-dev libgl1-mesa-dev zip unzip zlib1g-dev -y
-apt-get install -y vim wget net-tools locales bzip2 \
+apt-get install -y vim wget net-tools locales bzip2 python3.9 python3.9-dev \
     python-numpy #used for websockify/novnc
 #apt-get -y install git gcc curl make libxml2-utils flex m4 openjdk-8-jdk lib32stdc++6 libelf-dev libssl-dev python-enum34 python-mako syslinux-utils software-properties-common
 apt-get clean -y
