612d04063d750c1b2e0485e6a336d2bd40b88094
y0shimitsugh0st84 <68965220+y0shimitsugh0st84@users.noreply.github.com>
2021-09-28 20:53:47 +0200

    Update xfce_ui.sh

--- src/ubuntu/install/xfce_ui.sh
+++ src/ubuntu/install/xfce_ui.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 ### every exit != 0 fails the script
 set -e
-
+export DEBIAN_FRONTEND=noninteractive
 echo "Install Xfce4 UI components"
 apt-get update 
 apt-get install -y supervisor xfce4 xfce4-terminal xterm
@@ -9,9 +9,9 @@ apt-get purge -y pm-utils xscreensaver*
 apt install --fix-missing -y vim wget ca-certificates  pulseaudio xrdp\
   xauth supervisor uuid-runtime locales \
   openssh-server sudo git build-essential cmake libuv1-dev uuid-dev libmicrohttpd-dev libssl-dev \
-  nano python-pip  xvfb \
+  nano python3-pip  xvfb \
   python3-pip  locate  libxml2-dev libxslt1-dev libssl-dev libmicrohttpd-dev  \
-  libmysqlclient-dev byobu locate cron python-pyaudio python3-pyaudio ffmpeg \
+  libmysqlclient-dev byobu locate cron  python3-pyaudio ffmpeg \
   fonts-liberation libappindicator3-1 libfile-basedir-perl libfile-desktopentry-perl libfile-mimeinfo-perl \
   libindicator3-7  libipc-system-simple-perl libnet-dbus-perl libtie-ixhash-perl libx11-protocol-perl \
   libxml-parser-perl libxml-twig-perl libxml-xpathengine-perl xdg-utils  xserver-xephyr jq tor xarchiver libavcodec-extra vlc
